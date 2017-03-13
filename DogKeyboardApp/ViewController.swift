1cd774ab1baa05b77d1badc62473e7e3326db7f2
Sanny <shc1991@gatech.edu>
2017-03-12 23:01:58 -0400

    App folder commit
    Basic storyboard workflow.

---
+++ DogKeyboardApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DogKeyboardApp
+//
+//  Created by Sanny Chun on 3/12/17.
+//  Copyright © 2017 TeamSupreme. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
